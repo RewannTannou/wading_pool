c6bb371355ad512d6f0eb810e325b0de37edacd2
rewann nnn <rewann.tannou@epitech.eu>
2025-09-12 16:52:09 +0200

    modif nom fichier

--- jour3/tast2.3.py
+++
@@ -1,7 +0,0 @@
-#variable crée avec comme nom string et comme valeur hello world !
-string = " Hello world !"
-#Créer une nouvelle variable a partir de string et qui renvoie 0
-position = string . find ("a")
-#print 0
-print ( position )
-
